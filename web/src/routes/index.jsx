53f99ec5a495628768d1529ef5d668b212a02b28
Joao Vinicius <joaovvs@gmail.com>
2023-11-07 19:57:42 -0300

    project done

--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -9,9 +9,20 @@ import { SaleRoutes } from './sale.routes';
 
 import { AuthRoutes } from './auth.routes';
 import { USER_ROLE } from '../utils/roles';
+import { useEffect } from 'react';
+import { api } from '../services/api';
 
 export function Routes() {
-  const { user } = useAuth();
+  const { user, signOut } = useAuth();
+
+  useEffect(() => {
+    api.get('/users/validated')
+    .catch((error)=> {
+      if(error.response?.status === 401){
+      signOut();
+    }    
+  });
+  },[]);
 
   function AccessRoute(){
     switch(user.role){
